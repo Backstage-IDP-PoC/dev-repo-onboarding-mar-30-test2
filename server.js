11e69db7f1fe7944b53b6ffc4ce6a26b7d392a8d
Suresh <sureshmavp1@gmail.com>
2026-03-30 19:11:59 +0530

    Update server.js with onboarding message
    Added a welcome message for successful onboarding.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end('<body style="background:white;font-family:Arial;text-align:center;margin-top:40px;font-size:28px;">Welcome to GSPANN Self Service Portal!</body>');
+  res.end('<body style="background:white;font-family:Arial;text-align:center;margin-top:40px;font-size:28px;">You have successfully onboarded Developers Repo!</body>');
 });
 
 server.listen(3000, () => {
